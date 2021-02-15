e7f850bb1312e3becefb0f388dc0d7a53f01976e
Vlad Blazhko <mr_plum@mail.ru>
2021-02-15 09:12:23 +0100

    Add setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='biospectools',
+    version='0.1.0',
+    description='Python tools for processing spectral data',
+    author='BioSpecNorway Group',
+    author_email='?',
+    url='https://www.nmbu.no/en/faculty/realtek/research/groups/biospectroscopy',
+    packages=find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+    setup_requires=['pytest-runner', 'numpy'],
+    tests_require=['pytest']
+)
