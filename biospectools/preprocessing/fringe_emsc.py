6bfb22e2b2e8be0b23b3b66b1103b952f146881d
Vlad Blazhko <mr_plum@mail.ru>
2021-05-12 15:46:59 +0200

    Refactor

--- biospectools/preprocessing/fringe_emsc.py
+++ biospectools/preprocessing/fringe_emsc.py
@@ -36,22 +36,12 @@ class FringeEMSC:
 
     def transform(self, spectra):
         spectra = np.asarray(spectra)
-        wns = self.wavenumbers
         corrected = []
         emsc_models = []
         self.freqs_ = []
         for spec in spectra:
-            freqs = self._find_fringe_frequencies(spec, wns)
-            fringe_comps = np.array([sin_then_cos(freq * wns)
-                                     for freq in freqs
-                                     for sin_then_cos in [np.sin, np.cos]])
-            if self.constituents is not None:
-                constituents = np.concatenate((fringe_comps, self.constituents))
-            else:
-                constituents = fringe_comps
-            emsc = EMSC(
-                self.reference, wns, self.poly_order,
-                self.weights, constituents, self.scale)
+            freqs = self._find_fringe_frequencies(spec)
+            emsc = self._build_emsc(freqs)
             corr = emsc.transform(spec[None])
 
             corrected.append(corr[0])
@@ -71,12 +61,12 @@ class FringeEMSC:
         del self.freq_coefs_
         del self.freqs_
 
-    def _find_fringe_frequencies(self, raw_spectrum, wavenumbers):
-        region = self._select_fringe_region(raw_spectrum, wavenumbers)
+    def _find_fringe_frequencies(self, raw_spectrum):
+        region = self._select_fringe_region(raw_spectrum)
         region = region - region.mean()
         region *= self.window_function(len(region))
 
-        f_transform, freqs = self._apply_fft(region, wavenumbers)
+        f_transform, freqs = self._apply_fft(region)
         freq_idxs, _ = scipy.signal.find_peaks(f_transform)
 
         # use only N highest frequencies
@@ -92,13 +82,26 @@ class FringeEMSC:
         freq_idxs.sort()
         return freqs[freq_idxs]
 
-    def _apply_fft(self, region, wavenumbers):
+    def _apply_fft(self, region):
         k = self._padded_region_length(region)
-        dw = np.abs(np.diff(wavenumbers).mean())
+        dw = np.abs(np.diff(self.wavenumbers).mean())
         freqs = 2 * np.pi * scipy.fft.fftfreq(k, dw)[0:k // 2]
         f_transform = scipy.fft.fft(region, k)[0:k // 2]
         f_transform = np.abs(f_transform)
         return f_transform, freqs
+
+    def _build_emsc(self, freqs):
+        fringe_comps = np.array([sin_then_cos(freq * self.wavenumbers)
+                                 for freq in freqs
+                                 for sin_then_cos in [np.sin, np.cos]])
+        if self.constituents is not None:
+            constituents = np.concatenate((fringe_comps, self.constituents))
+        else:
+            constituents = fringe_comps
+        emsc = EMSC(
+            self.reference, self.wavenumbers, self.poly_order,
+            self.weights, constituents, self.scale)
+        return emsc
 
     def _padded_region_length(self, region):
         k = region.shape[-1]
@@ -108,46 +111,43 @@ class FringeEMSC:
             length += 1
         return length
 
-    def _select_fringe_region(self, spectra, wavenumbers):
+    def _select_fringe_region(self, spectra):
         """
         Assumes that spectra lies along last axis
         """
-        idx_lower = np.argmin(abs(wavenumbers - self.fringe_wn_location[0]))
-        idx_upper = np.argmin(abs(wavenumbers - self.fringe_wn_location[1]))
+        wns = self.wavenumbers
+        idx_lower = np.argmin(abs(wns - self.fringe_wn_location[0]))
+        idx_upper = np.argmin(abs(wns - self.fringe_wn_location[1]))
         if idx_lower > idx_upper:
             idx_lower, idx_upper = idx_upper, idx_lower
         region = spectra[..., idx_lower: idx_upper]
         return region
 
-    def _gather_emsc_attributes(self, emsc_models: List[EMSC]):
-        self.coefs_ = np.array(
-            [m.coefs_[0] for m in emsc_models])
-        self.scaling_coefs_ = np.array(
-            [m.scaling_coefs_[0] for m in emsc_models])
-        self.residuals_ = np.array(
-            [m.residuals_[0] for m in emsc_models])
+    def _gather_emsc_attributes(self, emscs: List[EMSC]):
+        self.coefs_ = np.array([e.coefs_[0] for e in emscs])
+        self.scaling_coefs_ = np.array([e.scaling_coefs_[0] for e in emscs])
+        self.residuals_ = np.array([e.residuals_[0] for e in emscs])
 
         if self.poly_order is not None:
-            all_poly = [m.polynomial_coefs_[0] for m in emsc_models]
-            self.polynomial_coefs_ = np.array(all_poly)
+            self.polynomial_coefs_ = np.array([e.polynomial_coefs_[0]
+                                               for e in emscs])
         else:
             self.polynomial_coefs_ = None
 
         if self.constituents is not None:
             n = len(self.constituents)
-            all_csts = [m.constituents_coefs_[0, -n:] for m in emsc_models]
-            self.constituents_coefs_ = np.array(all_csts)
+            self.constituents_coefs_ = np.array([e.constituents_coefs_[0, -n:]
+                                                 for e in emscs])
         else:
             self.constituents_coefs_ = None
 
-        self.freq_coefs_ = self._extract_frequencies(emsc_models)
+        self.freq_coefs_ = self._extract_frequencies(emscs)
 
-    def _extract_frequencies(self, emsc_models: List[EMSC]):
+    def _extract_frequencies(self, emscs: List[EMSC]):
         n = self.n_freq
         if self.double_freq:
             n *= 2
 
         # each freq has sine and cosine component
-        freq_coefs = [m.constituents_coefs_[0, :n * 2] for m in emsc_models]
-        freq_coefs = np.array(freq_coefs)
+        freq_coefs = np.array([e.constituents_coefs_[0, :n * 2] for e in emscs])
         return freq_coefs.reshape((-1, n, 2))
