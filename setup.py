90f82b701d001bfeb50148d357232edd6a6dc3d6
Vlad Blazhko <mr_plum@mail.ru>
2021-12-17 07:28:47 +0100

    Change version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='biospectools',
-    version='0.1.0',
+    version='0.2.0',
     description='Python tools for processing spectral data',
     author='BioSpecNorway Group',
     author_email='?',
