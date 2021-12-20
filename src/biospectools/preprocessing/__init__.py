8aab0dcdb1b7c47ded388c768f6905563d8bff7e
Vlad Blazhko <mr_plum@mail.ru>
2021-12-20 06:46:17 +0100

    Export internals

--- src/biospectools/preprocessing/__init__.py
+++ src/biospectools/preprocessing/__init__.py
@@ -1,4 +1,4 @@
 from .emsc import emsc  # noqa: F401
-from .emsc import EMSC  # noqa: F401
-from .fringe_emsc import FringeEMSC  # noqa: F401
-from .me_emsc import MeEMSC  # noqa: F401
+from .emsc import EMSC, EMSCInternals  # noqa: F401
+from .fringe_emsc import FringeEMSC, FringeEMSCInternals  # noqa: F401
+from .me_emsc import MeEMSC, MeEMSCInternals  # noqa: F401
