0704f96d519d8cbc79db0e950352b5376cb98052
Vlad Blazhko <mr_plum@mail.ru>
2022-09-06 21:38:37 +0200

    Add doi role to sphynx

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,6 +34,7 @@ extensions = [
     'myst_parser',
     'sphinx.ext.napoleon',
     'matplotlib.sphinxext.plot_directive',
+    'doi_role'
 ]
 
 napoleon_google_docstring = False
